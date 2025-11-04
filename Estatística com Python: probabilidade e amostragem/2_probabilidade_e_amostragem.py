62c31c63bee0fceb03dde4fcfcec978ae21ada64
Ligia <ligiacamara92@gmail.com>
2025-11-04 15:24:29 -0300

    final aula 2

--- Estatística com Python: probabilidade e amostragem/2_probabilidade_e_amostragem.py
+++ Estatística com Python: probabilidade e amostragem/2_probabilidade_e_amostragem.py
@@ -211,7 +211,164 @@ probabilidade
 
 # 3 - AMOSTRAGEM
 
-# 3.1 População e Amostra
+# 3.3 Amostragem Aleatória Simples
+
+dados.shape[0]
+
+# %%
+
+dados.Renda.mean()
+# %%
+
+amostra = dados.sample(n = 100, random_state=101)
+amostra.shape[0]
+# %%
+
+amostra.Renda.mean()
+# %%
+
+dados.Sexo.value_counts(normalize=True)
+# %%
+
+amostra.Sexo.value_counts(normalize=True)
+# %%
+
+# 4 - ESTIMAÇÃO
+
+#4.1 Teorema do Limite Central
+
+n = 2000
+total_de_amostras = 1500
+# %%
+
+amostras = pd.DataFrame()
+
+for i in range(total_de_amostras):
+    _ = dados.Idade.sample(n)
+    _.index = range(0, len(_))
+    amostras['Amostra_' + str(i)] = _
+
+amostras
+# %%
+
+amostras.mean()
+# %%
+
+amostras.mean().hist()
+# %%
+
+dados.Idade.mean()
+# %%
+
+amostras.mean().mean()
+# %%
+
+amostras.mean().std()
+# %%
+
+dados.Idade.std()
+# %%
+
+dados.Idade.std() / np.sqrt(n)
+# %%
+
+# 4.4 Intervalos de Confiança
+
+#Suponha que os pesos dos sacos de arroz de uma indústria alimentícia se distribuem aproximadamente como uma normal de **desvio padrão populacional igual a 150 g**. 
+#Selecionada uma **amostra aleatório de 20 sacos** de um lote específico, obteve-se um **peso médio de 5.050 g**. Construa um intervalo de confiança para a **média populacional** assumindo um **nível de significância de 5%**.
+
+media_amostra = 5050
+significancia = 0.05
+confianca = 1 - significancia
+
+tabela_normal_padronizada[16:26]
+# %%
+
+z = norm.ppf(0.975)
+z
+# %%
+
+desvio_padrao = 150
+n = 20
+raiz_de_n = np.sqrt(n)
+sigma = desvio_padrao/raiz_de_n
+
+sigma
+# %%
+
+e = z * sigma
+e
+# %%
+
+intervalo = (
+    media_amostra - e ,
+    media_amostra + e
+)
+intervalo
+# %%
+
+intervalo = norm.interval(confidence = 0.95, loc = media_amostra, scale = sigma)
+intervalo
+
+# %%
+
+# 5 - CÁLCULO DO TAMANHO DA AMOSTRA
+
+# Estamos estudando o rendimento mensal dos chefes de domicílios no Brasil. Nosso supervisor determinou que o **erro máximo em relação a média seja de R$\$$ 100,00**. 
+# Sabemos que o **desvio padrão populacional** deste grupo de trabalhadores é de **R$\$$ 3.323,39**. Para um **nível de confiança de 95%**, qual deve ser o tamanho da amostra de nosso estudo?
 
 
+z = norm.ppf(0.975)
+sigma = 3323.39
+e = 100
+n = (z * (sigma / e)) ** 2 
+int(n.round())
 
+# %%
+
+#Em um lote de **10.000 latas** de refrigerante foi realizada uma amostra aleatória simples de **100 latas** e foi obtido o **desvio padrão amostral do conteúdo das latas igual a 12 ml**. 
+#O fabricante estipula um **erro máximo sobre a média populacional de apenas 5 ml**. Para garantir um **nível de confiança de 95%** qual o tamanho de amostra deve ser selecionado para este estudo?
+
+N = 10000
+z = norm.ppf((0.5 +(0.95/2)))
+s = 12
+e = 5
+n = ((z**2)*(s**2)*(N)) / (((z**2)*(s**2))+((e**2)*(N-1)))
+int(n.round())
+
+# %%
+
+#Estamos estudando o **rendimento mensal dos chefes de domicílios com renda até R$\$$ 5.000,00 no Brasil**. Nosso supervisor determinou que o **erro máximo em relação a média seja de R$\$$ 10,00**. 
+#Sabemos que o **desvio padrão populacional** deste grupo de trabalhadores é de **R$\$$ 1.082,79** e que a **média populacional** é de **R$\$$ 1.426,54**. Para um **nível de confiança de 95%**, qual deve ser o tamanho da amostra de nosso estudo? 
+#Qual o intervalo de confiança para a média considerando o tamanho de amostra obtido?
+
+renda_5000 = dados.query('Renda <= 5000').Renda
+sigma = renda_5000.std()
+media = renda_5000.mean()
+
+# %%
+
+z = norm.ppf(.975)
+e = 10
+n = (z * (sigma / e)) ** 2 
+n = int(n.round())
+n
+# %%
+
+intervalo = norm.interval(confidence=0.95, loc = media, scale = sigma / np.sqrt(n))
+intervalo
+# %%
+
+tamanho_simulacao = 1000
+
+medias = [renda_5000.sample(n = n).mean() for i in range(1, tamanho_simulacao)]
+medias = pd.DataFrame(medias)
+
+ax = medias.plot(style = '.')
+ax.figure.set_size_inches(12, 6)
+ax.hlines(y = media, xmin = 0, xmax = tamanho_simulacao, colors='black', linestyles='dashed')
+ax.hlines(y = intervalo[0], xmin = 0, xmax = tamanho_simulacao, colors='red', linestyles='dashed')
+ax.hlines(y = intervalo[1], xmin = 0, xmax = tamanho_simulacao, colors='red', linestyles='dashed')
+ax
+
+# %%
